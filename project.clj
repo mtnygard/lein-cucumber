40e8ede1b3ff9307398e4b65f3ab828d81c384e0
Michael Nygard <mtnygard@gmail.com>
2018-04-02 15:24:43 -0500

    The plugin needs Clojure but shouldn't force it on consuming projects

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
 (defproject mtnygard/lein-cucumber "1.0.8-SNAPSHOT"
   :description "Run cucumber-jvm specifications with leiningen"
   :url https://github.com/mtnygard/lein-cucumber
-  :dependencies [[info.cukes/cucumber-clojure "1.2.5"]]
-  :profiles {:cucumber {:dependencies [[commons-io "2.4"]]
-                        :plugins [[mtnygard/lein-cucumber "1.0.8-SNAPSHOT"]]}
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
+                 [info.cukes/cucumber-clojure "1.2.5"]]
+  :profiles {:cucumber     {:dependencies [[commons-io "2.4"]]
+                            :plugins      [[mtnygard/lein-cucumber "1.0.8-SNAPSHOT"]]}
              :cuke-htmlrep [:cucumber {:cucumber
                                        {:formatter {:type :html
                                                     :path "target/report"}}}]}
   :eval-in :leiningen
-  :license {:name "Unlicense"
-            :url "http://unlicense.org/"
+  :license {:name         "Unlicense"
+            :url          "http://unlicense.org/"
             :distribution :repo})
